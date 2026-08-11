1b0a379f8dc289c78d220b87090b16c6e90d3c97
Christian NGAPGUE <ngapsgeschaft@gmail.com>
2026-08-11 16:00:31 +0100

    feat: Afficher le menu principal de l'application
    L'utilisateur peut choisir une action sur le menu proposé. En retour,
    l'application lui afficheson choix et la description de dernier.

--- bin/ffsc2026_projet_1_gestionnaire_de_taches.dart
+++ bin/ffsc2026_projet_1_gestionnaire_de_taches.dart
@@ -1,5 +1,6 @@
-import 'package:ffsc2026_projet_1_gestionnaire_de_taches/ffsc2026_projet_1_gestionnaire_de_taches.dart' as ffsc2026_projet_1_gestionnaire_de_taches;
+import 'package:ffsc2026_projet_1_gestionnaire_de_taches/ffsc2026_projet_1_gestionnaire_de_taches.dart'
+    as ffsc2026_projet_1_gestionnaire_de_taches;
 
 void main(List<String> arguments) {
-  print('Hello world: ${ffsc2026_projet_1_gestionnaire_de_taches.calculate()}!');
+  ffsc2026_projet_1_gestionnaire_de_taches.run();
 }
--- lib/ffsc2026_projet_1_gestionnaire_de_taches.dart
+++ lib/ffsc2026_projet_1_gestionnaire_de_taches.dart
@@ -1,3 +1,70 @@
+import 'dart:io';
+
 int calculate() {
   return 6 * 7;
+}
+
+void run() {
+  introduce();
+  while (true) {
+    printMenu();
+    print('Veuillez entrer votre choix :');
+    String? input = stdin.readLineSync();
+    try {
+      UserChoice choice = getUserChoice(input);
+      print('\nVous avez choisi ${choice.value}: ${choice.description}.');
+      if (choice == UserChoice.quit) {
+        quit();
+        return;
+      }
+    } on ChoiceException {
+      print('\nChoix "$input" invalide. Veuillez réessayer.');
+    }
+  }
+}
+
+enum UserChoice {
+  createTask(1, 'Créer une nouvelle tâche'),
+  listTasks(2, 'Lister toutes les tâches'),
+  markTaskCompleted(3, 'Marquer une tâche comme terminée'),
+  deleteTask(4, 'Supprimer une tâche'),
+  quit(5, 'Quitter le programme');
+
+  const UserChoice(this.value, this.description);
+
+  final int value;
+  final String description;
+}
+
+void printMenu() {
+  print('\n======== Gestionnaire de Tâches ========');
+  for (var choice in UserChoice.values) {
+    print('${choice.value}. ${choice.description}');
+  }
+}
+
+UserChoice getUserChoice(String? input) {
+  for (var choice in UserChoice.values) {
+    if (choice.value.toString() == input) {
+      return choice;
+    }
+  }
+  throw ChoiceException('Choix "$input" invalide');
+}
+
+class ChoiceException implements Exception {
+  final String message;
+  ChoiceException(this.message);
+
+  @override
+  String toString() => 'ChoiceException: $message';
+}
+
+void quit() {
+  print('Merci d\'avoir utilisé le gestionnaire de tâches. À bientôt !');
+}
+
+void introduce() {
+  print('\nBienvenue dans le gestionnaire de tâches !');
+  print('Ce programme vous aidera à gérer vos tâches efficacement.');
 }
