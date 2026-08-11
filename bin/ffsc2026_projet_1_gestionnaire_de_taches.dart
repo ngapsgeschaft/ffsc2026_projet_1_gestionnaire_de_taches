de8b66d8f7105acf0c07f4a8d59e265f3b33a01d
Christian NGAPGUE <ngapsgeschaft@gmail.com>
2026-08-11 11:23:00 +0100

    Initial commit

---
+++ bin/ffsc2026_projet_1_gestionnaire_de_taches.dart
@@ -0,0 +1,5 @@
+import 'package:ffsc2026_projet_1_gestionnaire_de_taches/ffsc2026_projet_1_gestionnaire_de_taches.dart' as ffsc2026_projet_1_gestionnaire_de_taches;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ffsc2026_projet_1_gestionnaire_de_taches.calculate()}!');
+}
---
+++ test/ffsc2026_projet_1_gestionnaire_de_taches_test.dart
@@ -0,0 +1,8 @@
+import 'package:ffsc2026_projet_1_gestionnaire_de_taches/ffsc2026_projet_1_gestionnaire_de_taches.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
