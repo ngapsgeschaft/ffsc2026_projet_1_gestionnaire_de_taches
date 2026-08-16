9bf3b0933542de47fa32ab100a4e9a253e8da6b7
Christian NGAPGUE <ngapsgeschaft@gmail.com>
2026-08-16 10:34:08 +0100

    feat: Create functions to execute menu actions

--- lib/ffsc2026_projet_1_gestionnaire_de_taches.dart
+++ lib/ffsc2026_projet_1_gestionnaire_de_taches.dart
@@ -1,88 +1,63 @@
 import 'dart:io';
 
-class TaskManager {
-  final List<Task> _tasks = [];
-
-  void addTask(Task aTask) {
-    _tasks.add(aTask);
-  }
-
-  Task getTaskById(String id) {
-    return _tasks.firstWhere((task) => task.id == id);
-  }
-
-  void removeTask(Task aTask) {
-    _tasks.remove(aTask);
-  }
-
-  List<Task> get tasks => List.unmodifiable(_tasks);
-
-  void markTaskCompleted(String aTaskId) {
-    final task = getTaskById(aTaskId);
-    task.isCompleted = true;
-  }
-}
-
-class Task {
-  final String id;
-  String title;
-  TaskPriority priority;
-  DateTime? dueDate;
-  bool isCompleted;
-
-  Task(
-    this.id,
-    this.title,
-    this.priority, {
-    this.dueDate,
-    this.isCompleted = false,
-  });
-}
-
-class UrgentTask extends Task {
-  UrgentTask(String id, String title, String description, {DateTime? dueDate})
-    : super(id, title, TaskPriority.high, dueDate: dueDate);
-}
-
-enum TaskPriority { low, medium, high }
-
 void run() {
   introduce();
-  while (true) {
+  bool terminate = false;
+  while (!terminate) {
     printMenu();
     print('Veuillez entrer votre choix :');
     String? input = stdin.readLineSync();
     try {
       UserChoice choice = getUserChoice(input);
       print('\nVous avez choisi ${choice.value}: ${choice.description}.');
-      if (choice == UserChoice.quit) {
-        quit();
-        return;
-      }
+      terminate = executeAndTerminate(choice);
     } on ChoiceException {
       print('\nChoix "$input" invalide. Veuillez réessayer.');
     }
   }
 }
 
-enum UserChoice {
-  createTask(1, 'Créer une nouvelle tâche'),
-  listTasks(2, 'Lister toutes les tâches'),
-  markTaskCompleted(3, 'Marquer une tâche comme terminée'),
-  deleteTask(4, 'Supprimer une tâche'),
-  quit(5, 'Quitter le programme');
-
-  const UserChoice(this.value, this.description);
-
-  final int value;
-  final String description;
+bool executeAndTerminate(UserChoice choice) {
+  if (choice == UserChoice.quit) {
+    return true;
+  } else {
+    switch (choice) {
+      case UserChoice.createTask:
+        createTask();
+        break;
+      case UserChoice.deleteTask:
+        deleteTask();
+        break;
+      case UserChoice.listTasks:
+        listTasks();
+        break;
+      case UserChoice.markTaskCompleted:
+        markAsComplete();
+        break;
+      default:
+    }
+    return false;
+  }
 }
 
-void printMenu() {
-  print('\n======== Gestionnaire de Tâches ========');
-  for (var choice in UserChoice.values) {
-    print('${choice.value}. ${choice.description}');
-  }
+void markAsComplete() {
+  print('Mark a task as completed.');
+}
+
+void listTasks() {
+  print('List all tasks.');
+}
+
+void deleteTask() {
+  print('Delete a task.');
+}
+
+void createTask() {
+  print('Create a task.');
+}
+
+void quit() {
+  print('Merci d\'avoir utilisé le gestionnaire de tâches. À bientôt !');
 }
 
 UserChoice getUserChoice(String? input) {
@@ -102,8 +77,24 @@ class ChoiceException implements Exception {
   String toString() => 'ChoiceException: $message';
 }
 
-void quit() {
-  print('Merci d\'avoir utilisé le gestionnaire de tâches. À bientôt !');
+void printMenu() {
+  print('\n======== Gestionnaire de Tâches ========');
+  for (var choice in UserChoice.values) {
+    print('${choice.value}. ${choice.description}');
+  }
+}
+
+enum UserChoice {
+  createTask(1, 'Créer une nouvelle tâche'),
+  listTasks(2, 'Lister toutes les tâches'),
+  markTaskCompleted(3, 'Marquer une tâche comme terminée'),
+  deleteTask(4, 'Supprimer une tâche'),
+  quit(5, 'Quitter le programme');
+
+  const UserChoice(this.value, this.description);
+
+  final int value;
+  final String description;
 }
 
 void introduce() {
